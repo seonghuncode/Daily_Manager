db3f2592ec783f0c7c0311db15278ea7c6b7eb66
seonghuncode <gnos456@naver.com>
2026-06-10 22:29:31 +0900

    feat:일기 관리 기능 구현(추가,수정,삭제)

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,127 @@
 import "./App.css";
+import { useReducer, useRef, createContext } from "react";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Edit from "./pages/Edit";
 import Notfound from "./pages/Notfound";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import { getEmotionImage } from "./util/get-emotion-image";
-import Button from "./components/Button";
-import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
 
-//페이지
-//1. "/" : 모든 일기를 조회하는 Home페이지
-//2. "/new" : 새로운 일기를 작성하는 New 페이지
-//3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+const mockData = [
+  {
+    id: 1,
+    createDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item,
+      );
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.id));
+    default:
+      return state;
+  }
+}
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
+
 function App() {
-  const nav = useNavigate();
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
 
-  const onClickButton = () => {
-    nav("/new");
+  //새로운 일기 추가
+  const onCreate = (createDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 수정
+  const onUpdate = (id, createDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  //기존 일기 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    });
   };
 
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"Right"} />}
-      />
-
-      <Button
-        text={"123"}
-        type={"DEFAULT"}
+      <button
         onClick={() => {
-          console.log("버튼 클릭");
+          onCreate(new Date().getTime(), 1, "Hello");
         }}
-      />
+      >
+        일기 추가 테스트
+      </button>
 
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
+      <button
         onClick={() => {
-          console.log("버튼 클릭");
+          onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다.");
         }}
-      />
+      >
+        일기 수정 테스트
+      </button>
 
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
+      <button
         onClick={() => {
-          console.log("버튼 클릭");
+          onDelete(1);
         }}
-      />
+      >
+        일기 삭제 테스트
+      </button>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
+      <DiaryStateContext.Provider value={data}>
+        {/* 상태변화 함수를 하위에서 공급받을 수 있다 */}
+        <DiaryDispatchContext.Provider
+          value={{
+            onCreate,
+            onDelete,
+            onUpdate,
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   );
 }
